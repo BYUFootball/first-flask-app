a4dd0bcb179b20f2886fa6c957f63df31714d253
BYUFootball <bensauer+bryant@gmail.com>
2016-09-09 17:56:13 -0700

    Update hello_app.py

--- hello_app.py
+++ hello_app.py
@@ -6,7 +6,7 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def hello():
-    return "my first its app its my own blog work in progress!"
+    return "https://www.google.com/search?q=pics+of+byu+logo&espv=2&biw=1280&bih=647&tbm=isch&imgil=-bSPBp8cHmj2xM%253A%253BhkvmBNlBOG2AYM%253Bhttp%25253A%25252F%25252Falvo.dvrlists.com%25252Fbyu-logo%25252F&source=iu&pf=m&fir=-bSPBp8cHmj2xM%253A%252ChkvmBNlBOG2AYM%252C_&usg=__ibdAZEyFWvSoEH_JCy2cXn_MAMw%3D&ved=0ahUKEwi8p-nDy4PPAhUB7GMKHa8wDLcQyjcINQ&ei=B1rTV_yHFIHYjwOv4bC4Cw#imgrc=-bSPBp8cHmj2xM%3A"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
